d516e4b4c54941c8d5e3c1b076d0055690810df8
steiger30 <renansteiger@live.com>
2022-03-16 23:12:27 -0300

    Configuração do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        semi:['error', 'always'],
+        quotes:['error', 'single'],
+        indent: ['error', 4],
+        'comma-spacing': ['error', { before: false, after: true}],
+    }
+};
