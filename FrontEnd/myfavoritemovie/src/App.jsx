a3c0d4b36162c860f0c5278861bed44623f9c87c
steiger30 <renansteiger@live.com>
2022-04-11 23:01:01 -0300

    Drawer (Inicio)

---
+++ FrontEnd/myfavoritemovie/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from './components/Navigation/NavBar'
+import Drawer from './components/Navigation/Drawer'
+
+function App() {
+  return (
+    <Drawer />
+  )
+}
+
+export default App
